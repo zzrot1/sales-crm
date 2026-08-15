64acdf38dbc8c078a4d9666a04f0416c6a7ebf2c
imihu <imihu97@gmail.com>
2026-08-15 21:10:11 +0300

    [Theme] - Game changing theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AppProviders } from "@/providers/app-providers";
-import { Outfit, Nunito_Sans, JetBrains_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 
-const outfit = Outfit({
+const geistSans = Geist({
   subsets: ["latin"],
-  variable: "--font-outfit",
+  variable: "--font-geist-sans",
 });
 
-const nunitoSans = Nunito_Sans({
+const geistMono = Geist_Mono({
   subsets: ["latin"],
-  variable: "--font-nunito-sans",
-});
-
-const jetbrainsMono = JetBrains_Mono({
-  subsets: ["latin"],
-  variable: "--font-jetbrains-mono",
+  variable: "--font-geist-mono",
 });
 export const metadata: Metadata = {
   title: "Sales CRM",
@@ -26,7 +21,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${outfit.variable} ${nunitoSans.variable} ${jetbrainsMono.variable}`}
+      className={`dark ${geistSans.variable} ${geistMono.variable}`}
     >
       <body className="typeset-app">
         <AppProviders>{children}</AppProviders>
